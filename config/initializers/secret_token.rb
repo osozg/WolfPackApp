fccf3882b96cbde3bc08dc3c1c2d779fdd571e81
Ozgür Osman <osman.ozg@gmail.com>
2013-04-13 14:51:11 +0100

    Initial Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WolfPackApp::Application.config.secret_token = '77a56a904af0194b55f0e7b1937b1f2dbafc22bb73d371cfb986af73dd13e15196bc92d7269fd6c1990d2a22efafe077986d210b8679eaa27fad2322e80eb868'
