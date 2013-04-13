e5f6fd74350d734164a5c0caf7294c0673aadeed
Ozgür Osman <osman.ozg@gmail.com>
2013-04-13 23:33:28 +0100

    resources

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,5 +1,6 @@
 class Resource < ActiveRecord::Base
   attr_accessible :category, :description, :wolf_pack_id, :url, :user_id, :votes
+  default_scope order('created_at DESC')
 
   belongs_to :wolf_pack
   # has_many :answers
