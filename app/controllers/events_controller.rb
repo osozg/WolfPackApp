8ccde48cca4ce967d930f44da92ac4e46828e118
Ozgür Osman <osman.ozg@gmail.com>
2013-04-14 03:00:16 +0100

    remove useless code

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,8 +1,4 @@
 class EventsController < ApplicationController
-  def index
-    # @wolf_pack = WolfPack.find(params[:wolf_pack_id])
-    # @events = @wolf_pack.events
-  end
 
   def show
     # @wolf_pack = WolfPack.find(params[:wolf_pack_id])
@@ -12,36 +8,4 @@ class EventsController < ApplicationController
     # end
   end
 
-  def new
-    @event = Event.new
-
-  end
-
-  def edit
-    @event = Event.find(params[:id])
-  end
-
-  def create
-    @event = Event.new(params[:event])
-    if @event.save
-      redirect_to @event, notice: 'Event was successfully created.'
-    else
-      render action: "new"
-    end
-  end
-
-  def update
-    @event = Event.find(params[:id])
-    if @event.update_attributes(params[:event])
-      redirect_to @event, notice: 'Event was successfully updated.'
-    else
-      render action: "edit"
-    end
-  end
-
-  def destroy
-    @event = Event.find(params[:id])
-    @event.destroy
-    redirect_to events_url
-  end
 end
